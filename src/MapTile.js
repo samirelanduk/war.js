1af4670ba033e432c52c21860decfe65bf6a8fbb
Sam Ireland <sam.ireland.uk@gmail.com>
2020-02-21 22:54:44 +0000

    Maps ENUM

--- src/MapTile.js
+++ src/MapTile.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
+import { MapEnum } from "./maps";
 import "./MapTile.scss"
 
 class MapTile extends Component {
     
     render() {
         const className = {
-            1: "sea",
-            2: "grass",
-            3: "trees",
-            4: "mountains",
-            5: "road",
-            6: "building",
-            7: "factory",
-            8: "HQ"
+            [MapEnum.SEA]: "sea",
+            [MapEnum.PLAINS]: "grass",
+            [MapEnum.TREES]: "trees",
+            [MapEnum.MOUNTAIN]: "mountains",
+            [MapEnum.ROAD]: "road",
+            [MapEnum.BRIDGE]: "bridge",
+            [MapEnum.BUILDINGS]: "building",
+            [MapEnum.FACTORY]: "factory",
+            [MapEnum.CAPITOL]: "HQ"
         }[this.props.type]
         return (
             <div className={"map-tile " + className}></div>
